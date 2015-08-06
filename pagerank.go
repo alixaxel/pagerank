e2ed2460bfc0e0d2d66e3a02e9dadd0b4e3f9e69
Alix Axel <alix.axel@gmail.com>
2015-08-06 04:31:07 +0200

    made members private

--- pagerank.go
+++ pagerank.go
@@ -7,48 +7,48 @@ import (
 	"math"
 )
 
-type _Node struct {
-	Weight   float64
-	Outbound float64
+type node struct {
+	weight   float64
+	outbound float64
 }
 
 type Graph struct {
-	Edges map[int](map[int]float64) // @TODO: This data structure is not ideal.
-	Nodes map[int]*_Node
+	edges map[int](map[int]float64) // @TODO: This data structure is not ideal.
+	nodes map[int]*node
 }
 
 // New initializes and returns a new graph.
 func New() *Graph {
 	return &Graph{
-		Edges: make(map[int](map[int]float64)),
-		Nodes: make(map[int]*_Node),
+		edges: make(map[int](map[int]float64)),
+		nodes: make(map[int]*node),
 	}
 }
 
 // Link creates a weighted edge between a source-target node pair.
 // If the edge already exists, the weight is incremented.
 func (self *Graph) Link(source, target int, weight float64) {
-	if _, ok := self.Nodes[source]; ok == false {
-		self.Nodes[source] = &_Node{
-			Weight:   0,
-			Outbound: 0,
+	if _, ok := self.nodes[source]; ok == false {
+		self.nodes[source] = &node{
+			weight:   0,
+			outbound: 0,
 		}
 	}
 
-	self.Nodes[source].Outbound += weight
+	self.nodes[source].outbound += weight
 
-	if _, ok := self.Nodes[target]; ok == false {
-		self.Nodes[target] = &_Node{
-			Weight:   0,
-			Outbound: 0,
+	if _, ok := self.nodes[target]; ok == false {
+		self.nodes[target] = &node{
+			weight:   0,
+			outbound: 0,
 		}
 	}
 
-	if _, ok := self.Edges[source]; ok == false {
-		self.Edges[source] = map[int]float64{}
+	if _, ok := self.edges[source]; ok == false {
+		self.edges[source] = map[int]float64{}
 	}
 
-	self.Edges[source][target] += weight
+	self.edges[source][target] += weight
 }
 
 // Rank computes the PageRank of every node in the directed graph.
@@ -58,59 +58,59 @@ func (self *Graph) Link(source, target int, weight float64) {
 // This method will run as many iterations as needed, until the graph converges.
 func (self *Graph) Rank(α, ε float64, callback func(id int, rank float64)) {
 	Δ := float64(1.0)
-	inverse := 1 / float64(len(self.Nodes))
+	inverse := 1 / float64(len(self.nodes))
 
 	// Normalize all the edge weights so that their sum amounts to 1.
-	for source := range self.Edges {
-		if self.Nodes[source].Outbound > 0 {
-			for target, _ := range self.Edges[source] {
-				self.Edges[source][target] /= self.Nodes[source].Outbound
+	for source := range self.edges {
+		if self.nodes[source].outbound > 0 {
+			for target, _ := range self.edges[source] {
+				self.edges[source][target] /= self.nodes[source].outbound
 			}
 		}
 	}
 
-	for key := range self.Nodes {
-		self.Nodes[key].Weight = inverse
+	for key := range self.nodes {
+		self.nodes[key].weight = inverse
 	}
 
 	for Δ > ε {
 		leak := float64(0)
 		nodes := map[int]float64{}
 
-		for key, value := range self.Nodes {
-			nodes[key] = value.Weight
+		for key, value := range self.nodes {
+			nodes[key] = value.weight
 
-			if value.Outbound == 0 {
-				leak += value.Weight
+			if value.outbound == 0 {
+				leak += value.weight
 			}
 
-			self.Nodes[key].Weight = 0
+			self.nodes[key].weight = 0
 		}
 
 		leak *= α
 
-		for source := range self.Nodes {
-			for target := range self.Edges[source] {
-				self.Nodes[target].Weight += α * nodes[source] * self.Edges[source][target]
+		for source := range self.nodes {
+			for target := range self.edges[source] {
+				self.nodes[target].weight += α * nodes[source] * self.edges[source][target]
 			}
 
-			self.Nodes[source].Weight += (1 - α) * inverse + leak * inverse
+			self.nodes[source].weight += (1 - α) * inverse + leak * inverse
 		}
 
 		Δ = 0
 
-		for key, value := range self.Nodes {
-			Δ += math.Abs(value.Weight - nodes[key])
+		for key, value := range self.nodes {
+			Δ += math.Abs(value.weight - nodes[key])
 		}
 	}
 
-	for key, value := range self.Nodes {
-		callback(key, value.Weight)
+	for key, value := range self.nodes {
+		callback(key, value.weight)
 	}
 }
 
 // Reset clears all the current graph data.
 func (self *Graph) Reset() {
-	self.Edges = make(map[int](map[int]float64))
-	self.Nodes = make(map[int]*_Node)
+	self.edges = make(map[int](map[int]float64))
+	self.nodes = make(map[int]*node)
 }
